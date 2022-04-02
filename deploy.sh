d52cb7a4077c008947bda172ab37f747a60b70fa
Olaf <olaf.ruvag@gmail.com>
2022-04-02 16:59:49 -0600

    deploy script added

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+node run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:olafruv/VUE-DeliCake-LaunchX.git master:gh-pages
+cd -
